bbc084c666a010cec525f1b17965b9a18c632509
yosef <yosefpineda@outlook.com>
2023-11-19 23:46:03 -0700

    Implementation of Login with MySQL Databasee

--- src/main/java/com/example/effortlogger/HelloApplication.java
+++ src/main/java/com/example/effortlogger/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("EffortLogger Tool");
+        stage.setTitle("EffortLogger Tool ");
         stage.setScene(scene);
         stage.show();
     }
